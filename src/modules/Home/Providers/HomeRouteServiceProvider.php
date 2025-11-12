fc699986bedf0584b0516de49cc7c0dc1a9ba31f
Doan LN <doanln16@gmail.com>
2025-11-13 01:09:46 +0700

    refactor: bỏ controller class trong route action vì đã khai báo ở trên
    - Thay [HomeController::class, 'method'] thành chỉ 'method'
    - Controller đã được khai báo trong ->controller(HomeController::class)

--- src/modules/Home/Providers/HomeRouteServiceProvider.php
+++ src/modules/Home/Providers/HomeRouteServiceProvider.php
@@ -10,11 +10,11 @@ class HomeRouteServiceProvider extends ServiceProvider
 {
     public function register(){
         System::context('web')->module(['slug' => 'home', 'prefix' => '/', 'priority' => 1])->controller(HomeController::class)->as('home')->group(function ($module) {
-            $module->get('/', [HomeController::class, 'index'])->name('index')->view('web.home');
-            $module->get('/about', [HomeController::class, 'about'])->name('about')->view('web.about');
-            $module->get('/contact', [HomeController::class, 'contact'])->name('contact')->view('web.contact');
-            $module->get('/docs', [HomeController::class, 'docs'])->name('docs')->view('web.docs');
-            $module->get('/examples', [HomeController::class, 'examples'])->name('examples')->view('web.examples');
+            $module->get('/', 'index')->name('index')->view('web.home');
+            $module->get('/about', 'about')->name('about')->view('web.about');
+            $module->get('/contact', 'contact')->name('contact')->view('web.contact');
+            $module->get('/docs', 'docs')->name('docs')->view('web.docs');
+            $module->get('/examples', 'examples')->name('examples')->view('web.examples');
         });
     }
 }
